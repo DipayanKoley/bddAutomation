cc8ee62fb53203b4888d87be71cde9574e0a5d7d
dipayankoley <dipayankoley@gmail.com>
2019-01-22 23:23:57 +0530

    Constant class addition

--- src/main/java/utility/Constant.java
+++ src/main/java/utility/Constant.java
@@ -4,6 +4,6 @@ public class Constant
 {
 	public final static String driverPath = "C:\\Users\\dell\\Desktop\\Others\\Selenium\\chromedriver.exe";
 	public final static String url = "https://www.phptravels.net/";
-	public final static String excelFile = "";
+	public final static String excelFile = "C:\\Users\\dell\\git\\bddAutomation\\src\\main\\java\\utility\\TestData.xlsx";
 	public final static String excelSheet = "Data";
 }
